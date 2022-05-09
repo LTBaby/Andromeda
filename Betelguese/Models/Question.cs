c7651f28b3d05e6b4940a7a12bfd7c5f5641ca14
LT69Baby <trevor998tredoux@gmail.com>
2022-05-09 21:32:52 +0200

    Qna setup

---
+++ Betelguese/Models/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace Betelguese.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Betelguese/Models/UserQuestionAnswer.cs
@@ -0,0 +1,10 @@
+﻿namespace Betelguese.Models
+{
+    public class UserQuestionAnswer
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int QuestionId { get; set; }
+        public int AnswerId { get; set; }
+    }
+}
